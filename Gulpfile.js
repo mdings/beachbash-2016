cb245ae75b8488832aed67aac213a0dd906ab633
Maarten Dings <maartendings@gmail.com>
2016-03-28 20:23:07 +0200

    Changes towards 1.0

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,8 +1,10 @@
-var gulp          = require('gulp');
-var browserSync   = require('browser-sync').create();
-var sass          = require('gulp-sass');
-var postcss       = require('gulp-postcss');
-var autoprefixer  = require('autoprefixer');
+var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+var sass = require('gulp-sass');
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+var fontMagician = require('postcss-font-magician');
+var concat = require('gulp-concat');
 
 // Static Server + watching scss/html files
 gulp.task('serve', function() {
@@ -11,30 +13,43 @@ gulp.task('serve', function() {
     server: 'build'
   });
   gulp.watch('static/**/*.scss', ['sass']);
+  gulp.watch('static/**/*.js', ['scripts']);
   gulp.watch('static/img/**/*.{gif, jpg, jpeg, .png}', ['build']);
   gulp.watch('documents/**/*.md', ['build']);
   gulp.watch('layouts/**/*.jade', ['build']);
+});
+
+gulp.task('scripts', function() {
+  return gulp.src([
+    'bower_components/fancybox/source/jquery.fancybox.js',
+    'bower_components/fancybox/source/helpers/jquery.fancybox-media.js',
+    'static/js/main.js'
+  ])
+  .pipe(concat('main.js'))
+  .pipe(gulp.dest('build/js/'))
+  .pipe(browserSync.stream());
 });
 
 // Compile sass into CSS & auto-inject into browsers
 gulp.task('sass', function() {
   var processors = [
     autoprefixer({browsers: ['last 2 version']}),
+    fontMagician({hosted: './static/fonts/'})
   ];
   return gulp.src('static/css/*.scss')
-    .pipe(sass({
-      includePaths: ['./bower_components/foundation-sites/scss/']
-    }))
-    .on('error', sass.logError)
-    .pipe(postcss(processors))
-    .pipe(gulp.dest('build/css/'))
-    .pipe(browserSync.stream());
+  .pipe(sass({
+    includePaths: ['./bower_components/foundation-sites/scss/', './bower_components/']
+  }))
+  .on('error', sass.logError)
+  .pipe(postcss(processors))
+  .pipe(gulp.dest('build/css/'))
+  .pipe(browserSync.stream());
 });
 
 gulp.task('build', function(){
   var ms = require('./metalsmith');
   ms('build').build(function(){
-      browserSync.reload({stream:false});
+    browserSync.reload({stream:false});
   });
 });
 
---
+++ static/js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  $('.panel__video').fancybox({
+    openEffect  : 'none',
+		closeEffect : 'none',
+    margin: 40,
+		helpers : {
+			media : {}
+		}
+  });
+});
