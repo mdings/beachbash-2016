dbe95b3c24c3e3dc03630efe6f66244588e978a5
Maarten Dings <maartendings@gmail.com>
2016-04-10 22:35:25 +0200

    Building scroll functionality

--- static/js/main.js
+++ static/js/main.js
@@ -1,29 +1,48 @@
 $(document).ready(function() {
 
-  $('.aftermovie').fancybox({
-    openEffect  : 'none',
-		closeEffect : 'none',
-    margin: 40,
-		helpers : {
-			media : {}
-		}
+  var $panes = $('.panel');
+  var $scrollPoints = new Array();
+  var $offset = 0;
+  var $index = 0;
+  var $lastScrollTop = 0;
+
+  $panes.each(function(i, elm){
+    var nHeight = (i > 0) ? ($(this).outerHeight() + $scrollPoints[i-1]) : $(this).outerHeight();
+    $scrollPoints.push(nHeight);
   });
 
-  // handle the fixed backgrounds like a MF
-  $(window).scroll(function(){
+  // prepend 0 to the array
+  $scrollPoints.unshift(0);
 
-    var $panelTout = $('.panel-tout');
-    var $panelIntro = $('.panel-intro');
-    var $panelPayoff = $('.panel-payoff');
-    var $panelQA = $('.panel-qa');
-    var $panelLineUp = $('.panel-line-up');
-    var $panelExit = $('.panel-exit');
+  // set the scrollable page
+  $('.scroller').height($scrollPoints[$scrollPoints.length - 1]);
 
-    $panelTout.toggleClass('is-offscreen', $(this).scrollTop() > $panelIntro.offset().top)
+  $(window).on('scroll', function(e) {
+    var $win = $(this);
+
+
+    var scrollOffset = ($win.scrollTop() - $scrollPoints[$index]) * -1;
+    scrollOffset = scrollOffset < 0 ? scrollOffset : 0; // scrollOffset can never be smaller than 0!
+    $($panes[$index]).css('transform', 'translateY(' + scrollOffset  + 'px)');
+
+
+
+    if ($win.scrollTop() > $lastScrollTop){
+      if( $win.scrollTop() > $scrollPoints[$index+1]) {
+        $index++;
+
+      }
+    } else {
+      if( $win.scrollTop() < $scrollPoints[$index]) {
+        $index--;
+      }
+    }
+    $lastScrollTop = $win.scrollTop();
+
+    // if( $win.scrollTop() > $scrollPane.height() ) {
+    //   $offset = $scrollPane.height();
+    //   $scrollPane = $('.panel.two');
+    // }
 
   }).trigger('scroll');
-
-  setInterval(function(){
-    $('.panel-payoff').toggleClass('is-inverse');
-  }, 50);
 });
